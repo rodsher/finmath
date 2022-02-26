95563135b5af62b47a49b3d6b2660f247112b2e7
Max Isaev <isaevm2@yandex.ru>
2022-02-26 19:05:34 +0300

    feat(src): Create scaffold for method and add empty tests

--- src/finmath.ts
+++ src/finmath.ts
@@ -1,6 +1,38 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-export default class DummyClass {
+// import "core-js/fn/array.find"
 
+export default class Math {
+  private leftOperand: number
+  private rightOperand!: number
+
+  constructor()
+  constructor(num: number)
+  constructor(num?: number) {
+    this.leftOperand = num ?? 0
+  }
+
+  static fromNumber(num: number): Math {
+    return new Math(num)
+  }
+
+  // static fromString(num: string): Finmath
+
+  // randomFloat()
+  // randomBetween()
+  // randomInt()
+  // randomIntBetween()
+  // round()
+  // floor()
+  // sign()
+  // pow(x, y)
+  // max()
+  // min()
+  // trunc()
+  // toString()
+  // toPrecision()
+
+  // add()
+  // sub()
+  // mul()
+  // div()
 }
--- test/finmath.test.ts
+++ test/finmath.test.ts
@@ -1,14 +1,9 @@
-import DummyClass from "../src/finmath"
-
-/**
- * Dummy test
- */
-describe("Dummy test", () => {
-  it("works if true is truthy", () => {
-    expect(true).toBeTruthy()
-  })
-
-  it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+describe('Math', () => {
+  describe('fromString', () => {
+    it('should throw exception when given string is not valid number', () => {})
+    it('should pass validation when given string is a valid integer', () => {})
+    it('should pass validation when given string is a valid float', () => {})
+    it('should pass validation when given string is a positive number', () => {})
+    it('should pass validation when given string is a negative number', () => {})
   })
 })
