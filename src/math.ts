ef963957bbb4043c017569dc53ff2b9f02b1f2e5
Max Isaev <m.y.isaev@tinkoff.ru>
2022-03-09 22:59:08 +0300

    feat(math): Create module math

---
+++ src/math.ts
@@ -0,0 +1,107 @@
+/**
+ * This is a main library file that containts a primary class Math.
+ *
+ * @fileoverview
+ */
+
+// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
+// import "core-js/fn/array.find"
+
+/* tslint:disable:unified-signatures */
+
+import { ArgumentIsGreaterThanMaxError, ArgumentIsLessThanMinError } from './exceptions'
+import { NumberPrecision } from './number-precision'
+import { floatToBigInt } from './utilities'
+
+export class FinMath {
+  private leftOperand: number
+  private rightOperand!: number
+  private precision!: NumberPrecision
+
+  constructor()
+  constructor(num: number)
+  constructor(num?: number) {
+    this.leftOperand = num ?? 0
+    this.precision = new NumberPrecision()
+  }
+
+  static fromNumber(num: number): FinMath {
+    if (num > Number.MAX_SAFE_INTEGER) {
+      throw new ArgumentIsGreaterThanMaxError(num)
+    }
+
+    if (num < Number.MIN_SAFE_INTEGER) {
+      throw new ArgumentIsLessThanMinError(num)
+    }
+
+    return new FinMath(num)
+  }
+
+  // Planning static methods:
+  // - fromString
+  // - add
+  // - sub
+  // - mul
+  // - div
+
+  setLeftOperand(num: number | string): FinMath {
+    this.leftOperand = Number(num)
+
+    return this
+  }
+
+  setRightOperand(num: number | string): FinMath {
+    this.rightOperand = Number(num)
+
+    return this
+  }
+
+  add(num: number): FinMath
+  add(num: string): FinMath
+  add(num: number | string): FinMath {
+    this.setRightOperand(num)
+
+    const maxPrecision = this.precision.max(this.leftOperand, this.rightOperand)
+    const multiplier = 10 ** maxPrecision
+    const leftOperand = floatToBigInt(this.leftOperand, multiplier)
+    const rightOperand = floatToBigInt(this.rightOperand, multiplier)
+
+    const result = Number(leftOperand + rightOperand) / multiplier
+
+    return this.setLeftOperand(result)
+  }
+
+  mul(num: number): FinMath
+  mul(num: string): FinMath
+  mul(num: number | string): FinMath {
+    this.setRightOperand(num)
+
+    const maxPrecision = this.precision.max(this.leftOperand, this.rightOperand)
+    const multiplier = 10 ** maxPrecision
+    const leftOperand = floatToBigInt(this.leftOperand, multiplier)
+    const rightOperand = floatToBigInt(this.rightOperand, multiplier)
+
+    const result = Number(leftOperand * rightOperand) / multiplier ** 2
+
+    return this.setLeftOperand(result)
+  }
+
+  /**
+   * Returns the result of operations.
+   * The default value will be returned when operations never called.
+   *
+   * @returns Result of arithmetical operations
+   */
+  result(): number {
+    return this.leftOperand
+  }
+
+  /**
+   * Returns a result of operations as number with string representation.
+   *
+   * @returns Result of arithmetical operations
+   */
+  resultAsString(): string {
+    return this.leftOperand.toString()
+  }
+}
