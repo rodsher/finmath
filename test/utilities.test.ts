5509dbb250a203589fcb086f3da0cb7676c97a3e
Max Isaev <m.y.isaev@tinkoff.ru>
2022-03-09 22:51:28 +0300

    test(utilities): Add unit tests for func floatToBigInt

--- test/utilities.test.ts
+++ test/utilities.test.ts
@@ -1,4 +1,5 @@
-import { diff } from '../src/utilities'
+import * as assert from 'assert'
+import { diff, floatToBigInt } from '../src/utilities'
 
 describe('utilities', () => {
   describe('diff', () => {
@@ -32,6 +33,42 @@ describe('utilities', () => {
 
     it('should return a right result when first and second arguments are negative numbers, second greater', async () => {
       expect(diff(-20, -10)).toBe(10)
+    })
+  })
+
+  describe('floatToBigInt', () => {
+    // In this describe section, we are using assert module because
+    // Jest tries to stringify BigInt that failures test cases.
+    // See error: "TypeError: Do not know how to serialize a BigInt"
+
+    it('should return zero when input also equals zero', () => {
+      assert.strictEqual(floatToBigInt(0, 1), BigInt(0))
+    })
+
+    it('should return a right bigint when inputs are positive numbers', () => {
+      assert.strictEqual(floatToBigInt(1, 10), BigInt(10))
+      assert.strictEqual(floatToBigInt(2.0, 10), BigInt(20))
+      assert.strictEqual(floatToBigInt(3.25, 100), BigInt(325))
+      assert.strictEqual(floatToBigInt(0.00007, 10 ** 7), BigInt(700))
+    })
+
+    it('should return a right bigint when inputs are negative numbers', () => {
+      assert.strictEqual(floatToBigInt(-1, 10), BigInt(-10))
+      assert.strictEqual(floatToBigInt(-2.0, 10), BigInt(-20))
+      assert.strictEqual(floatToBigInt(-3.25, 100), BigInt(-325))
+      assert.strictEqual(floatToBigInt(-0.00007, 10 ** 7), BigInt(-700))
+    })
+
+    it('should throw exception when precision is negative', () => {
+      expect(() => floatToBigInt(0.1, -1)).toThrowError(
+        'Multiplier must be > 0, given -1 is less or equal to zero'
+      )
+    })
+
+    it('should throw exception when precision equals to zero', () => {
+      expect(() => floatToBigInt(0.1, 0)).toThrowError(
+        'Multiplier must be > 0, given 0 is less or equal to zero'
+      )
     })
   })
 })
