88f7fa37b988a8572cff9553c3a8b579caebc383
Max Isaev <m.y.isaev@tinkoff.ru>
2022-03-09 22:49:11 +0300

    feat(src): Add exceptions for invalid numbers and multpliers

--- src/exceptions.ts
+++ src/exceptions.ts
@@ -8,9 +8,10 @@
 /* istanbul ignore file */
 
 /**
- * ArgumentIsGreaterThanMaxError throws when a given argument is greater than a constant Number.MAX_SAFE_INTEGER.
+ * The ArgumentIsGreaterThanMaxError throws when a given argument is greater than a constant Number.MAX_SAFE_INTEGER.
  * The value of Number.MAX_SAFE_INTEGER is 9007199254740991 (2^53 − 1).
  *
+ * @class
  * @public
  */
 export class ArgumentIsGreaterThanMaxError extends Error {
@@ -24,9 +25,10 @@ export class ArgumentIsGreaterThanMaxError extends Error {
 }
 
 /**
- * ArgumentIsLessThanMinError throws when a given argument is less than a constant Number.MIN_SAFE_INTEGER.
+ * The ArgumentIsLessThanMinError throws when a given argument is less than a constant Number.MIN_SAFE_INTEGER.
  * The value of Number.MIN_SAFE_INTEGER is −9007199254740991, (−(2^53 − 1)).
  *
+ * @class
  * @public
  */
 export class ArgumentIsLessThanMinError extends Error {
@@ -36,5 +38,54 @@ export class ArgumentIsLessThanMinError extends Error {
    */
   constructor(num: number) {
     super(`Given argument is less than a MIN_SAFE_INTEGER: ${num} < ${Number.MIN_SAFE_INTEGER}`)
+  }
+}
+
+/**
+ * The InvalidNumberError throws when a given value had invalid format.
+ *
+ * Valid formars for numbers:
+ * - 100
+ * - +100
+ * - -100
+ * - 100.0
+ * - +100.0
+ * - -100.0
+ *
+ * Valid formats for string representation of numbers:
+ * - '100'
+ * - '+100'
+ * - '-100'
+ * - '100.0'
+ * - '+100.0'
+ * - '-100.0'
+ *
+ * @class
+ * @public
+ */
+export class InvalidNumberError extends Error {
+  /**
+   * @constructor
+   * @param num Invalid value.
+   */
+  constructor(num: number | string) {
+    super(`Given argument ${num} is not a valid integer or decimal`)
+  }
+}
+
+/**
+ * The InvalidBigIntMultiplierError throws when given multiplier is less than 1.
+ * Multiplier for bigint must be greater or equal than zero.
+ *
+ * @class
+ * @public
+ */
+export class InvalidBigIntMultiplierError extends Error {
+  /**
+   * @constructor
+   * @param num Multiplier value.
+   */
+  constructor(num: number | string) {
+    super(`Multiplier must be > 0, given ${num} is less or equal to zero`)
   }
 }
