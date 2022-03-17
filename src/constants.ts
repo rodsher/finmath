2d80dbac7bdeadecd09ea842b3da20dbe83fc34d
Max Isaev <m.y.isaev@tinkoff.ru>
2022-03-17 10:41:25 +0300

    feat(src): Create file with library constants

---
+++ src/constants.ts
@@ -0,0 +1,23 @@
+/**
+ * File contains library constants.
+ * Code coverage disabled for this file because it does not contain conditional logic or branches.
+ *
+ * @fileoverview
+ */
+/* istanbul ignore file */
+
+/**
+ * Describes a precision value for integers.
+ *
+ * @example
+ * // Example of usage constant:
+ * const precision = new NumberPrecision()
+ *
+ * if (precision.compute(100) === ZERO_PRECISION) {
+ *   ...
+ * }
+ *
+ * @constant
+ * @public
+ */
+export const ZERO_PRECISION = 0
