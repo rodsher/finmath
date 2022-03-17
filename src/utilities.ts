eaedd63a49bfe2c4eaab6e7fad662ebfb3310c6d
Max Isaev <m.y.isaev@tinkoff.ru>
2022-03-17 10:54:10 +0300

    feat(src): Copy function scientificNotationToFloat without refactoring

--- src/utilities.ts
+++ src/utilities.ts
@@ -2,6 +2,16 @@ import { InvalidBigIntMultiplierError } from './exceptions'
 
 /**
  * diff computes a positive difference between two numbers.
+ * Always return a positive value.
+ *
+ * @example
+ * // Examples:
+ * diff(5, 10) // 5
+ * diff(10, 5) // 5
+ * diff(-5, 10) // 15
+ * diff(-10, 5) // 15
+ * diff(-10, -20) // 10
+ * diff(-20, -10) // 10
  *
  * @param x First number
  * @param y Second number
@@ -64,4 +74,50 @@ export function floatToBigInt(num: number, multiplier: number): bigint {
   const roundedToNearestInteger = Math.round(value)
 
   return BigInt(roundedToNearestInteger)
+}
+
+export function scientificNotationToFloat(num: number | string) {
+  const nsign = Math.sign(num as number)
+  //remove the sign
+  num = Math.abs(num as number)
+
+  //if the number is in scientific notation remove it
+  if (/\d+\.?\d*e[\+\-]*\d+/i.test(num.toString())) {
+    var zero = '0',
+      parts = String(num)
+        .toLowerCase()
+        .split('e'), //split into coeff and exponent
+      e = parts.pop(), //store the exponential part
+      //@ts-ignore
+      l = Math.abs(e), //get the number of zeros
+      //@ts-ignore
+      sign = e / l,
+      coeff_array = parts[0].split('.')
+    if (sign === -1) {
+      l = l - coeff_array[0].length
+      if (l < 0) {
+        //@ts-ignore
+        num =
+          coeff_array[0].slice(0, l) +
+          '.' +
+          coeff_array[0].slice(l) +
+          (coeff_array.length === 2 ? coeff_array[1] : '')
+      } else {
+        //@ts-ignore
+        num = zero + '.' + new Array(l + 1).join(zero) + coeff_array.join('')
+      }
+    } else {
+      var dec = coeff_array[1]
+      if (dec) l = l - dec.length
+      if (l < 0) {
+        //@ts-ignore
+        num = coeff_array[0] + dec.slice(0, l) + '.' + dec.slice(l)
+      } else {
+        //@ts-ignore
+        num = coeff_array.join('') + new Array(l + 1).join(zero)
+      }
+    }
+  }
+
+  return nsign < 0 ? '-' + num : num.toString()
 }
